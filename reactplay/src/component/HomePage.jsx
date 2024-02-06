f9c80c983a848deb350a291cbd1b4fa79c8c8d9b
RusulAzomSumon <18103020@iubat.edu>
2024-02-06 21:46:53 +0600

    home U

--- reactplay/src/component/HomePage.jsx
+++
@@ -1,6 +0,0 @@
-import HomeViewPage from '../pages/HomeViewPage';
-<HomeViewPage
-  title="Welcome to My Website!"
-  content="This is the homepage of my amazing website."
-  image="https://example.com/hero-image.jpg"
-/>
--- reactplay/src/pages/HomeViewPage.jsx
+++ reactplay/src/pages/HomeViewPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 function HomeViewPage() {
   return (
-    <div className="home-page">
-      <h1>{title}</h1>
-      {image && <img src={image} alt={title} />}
-      <p>{content}</p>
+    <div>
+      <h1>Wellcome to Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore impedit quas quod quasi accusantium facilis deleniti rerum reiciendis earum id tempora, voluptates quam natus quia! Iure quis tempore, voluptas mollitia eos dignissimos quos beatae sunt alias consequatur fugit earum libero, laudantium fuga dicta id delectus reiciendis sint ipsam quam molestiae!
+      </p>
     </div>
   );
 }
